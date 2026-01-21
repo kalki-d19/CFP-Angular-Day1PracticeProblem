9e1f98f5026499a35b1733ef4a76df9a560e32f9
dkalki-19 <dkalkicdhandapani@gmail.com>
2026-01-21 14:33:53 +0530

    UC3 - To launch Bridgelabz Site in a new Tab on clicking Bridgelabz Logo

--- HELLO-WORLD/src/app/app.ts
+++ HELLO-WORLD/src/app/app.ts
@@ -1,16 +1,23 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  title = "Helloworld";
-  imgUrl = "blLogo.jpg";
-  ngOnInit():void{
-    this.title= "Hello from bridgelabz.";
+
+  title = 'Helloworld';
+  imgUrl = 'blLogo.jpg';
+  url = 'https://www.bridgelabz.com';
+
+  ngOnInit(): void {
+    this.title = 'Hello from bridgelabz.';
+  }
+  onClick(event: any) {
+    console.log('Save button is Clicked!', event);
+    window.open(this.url, '_blank');
   }
 }
